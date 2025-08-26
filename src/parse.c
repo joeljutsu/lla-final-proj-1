5367e9654cede8af62439e2f85aebb7be0ad9858
joeljutsu <joeljutsu@outlook.com>
2025-08-26 19:26:07 +0800

    placeholder function bodies for the  func sigs in parse.h

---
+++ src/parse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int create_db_header(int fd, struct dbheader_t **header_out)
+{
+
+    return 0;
+}
+int validate_db_header(int fd, struct dbheader_t **header_out)
+{
+    return 0;
+}
+int read_employees(int fd, struct dbheader_t *, struct employee_t **employees_out)
+{
+    return 0;
+}
+int output_file(int fd, struct dbheader_t *, struct employee_t **employees)
+{
+
+    return 0;
+}
