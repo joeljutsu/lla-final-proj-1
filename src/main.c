b10b195617b640b4988b6228069b176d774e2a93
joeljutsu <joeljutsu@outlook.com>
2025-08-26 16:40:12 +0800

    added getopt, w opts for n and f. added print usage

--- src/main.c
+++ src/main.c
@@ -6,7 +6,46 @@
 #include "file.h"
 #include "parse.h"
 
+void print_usage(char *argv[])
+{
+    printf("Usage: %s -n -f <database file> \n", argv[0]);
+    printf("\t -n - create new database file \n");
+    printf("\t -f - (recquired) path to database file \n");
+    return;
+}
+
 int main(int argc, char *argv[])
 {
+    char *filepath = NULL;
+    bool newfile = false;
+    int c = 0;
+
+    while ((c = getopt(argc, argv, "nf:")) != -1)
+    {
+        switch (c)
+        {
+        case 'n':
+            newfile = true;
+            printf("run with n flag\n");
+            break;
+        case 'f':
+            printf("run w f flag\n");
+            filepath = optarg;
+            break;
+        case '?':
+            printf("Uknown option -%c\n", c);
+            break;
+        default:
+            return -1;
+        }
+    }
+
+    if (filepath == NULL)
+    {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        return 0;
+    }
+
     return 0;
 }
