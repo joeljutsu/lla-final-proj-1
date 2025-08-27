97e30174b0266e8346e7396ea6b678e21e1f8ca0
joeljutsu <joeljutsu@outlook.com>
2025-08-27 17:56:08 +0800

    removed fd from create db header

--- src/parse.c
+++ src/parse.c
@@ -80,6 +80,26 @@ int validate_db_header(int fd, struct dbheader_t **header_out)
     return STATUS_SUCCESS;
 }
 
+int create_db_header(struct dbheader_t **header_out)
+{
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL)
+    {
+        printf("calloc failed to create db header\n");
+        return STATUS_ERROR;
+    }
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *header_out = header;
+
+    return STATUS_SUCCESS;
+}
+
+/*
+
 int create_db_header(int fd, struct dbheader_t **header_out)
 {
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
@@ -97,8 +117,6 @@ int create_db_header(int fd, struct dbheader_t **header_out)
 
     return STATUS_SUCCESS;
 }
-/*
-
 int read_employees(int fd, struct dbheader_t *, struct employee_t **employees_out)
 {
     return 0;
